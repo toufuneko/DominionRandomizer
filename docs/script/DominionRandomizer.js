38b5f7b38e667a8d8619c8005094729bcade3603
toufuneko <38408925+toufuneko@users.noreply.github.com>
2018-04-16 07:13:49 +0900

    test2
    test2

---
+++ docs/script/DominionRandomizer.js
@@ -0,0 +1,419 @@
+//ランダマイザ―カードリスト
+
+//帝国
+var Teikoku = {
+  "ExtensionName":"帝国",
+  "Cards": [
+    {
+      "Name":["生贄"],
+      "Cost":[4]
+    },
+    {
+      "Name":["石", "投石機"],
+      "Cost":[4,2]
+    },
+    {
+      "Name":["ヴィラ"],
+      "Cost":[4]
+    },
+    {
+      "Name":["パトリキ","エンポリウム"],
+      "Cost": [2,5]
+    },
+    {
+      "Name":["王室の鍛冶屋"],
+      "Cost": [-8]
+    },
+    {
+      "Name":["お守り"],
+      "Cost":5
+    },
+    {
+      "Name":["女魔術師"],
+      "Cost":[3]
+    },
+    {
+      "Name":["開拓者", "騒がしい村"],
+      "Cost": [2,5]
+    },
+    {
+      "Name":["冠"],
+      "Cost": [5]
+    },
+    {
+      "Name":["技術者"],
+      "Cost":[-4]
+    },
+    {
+      "Name":["軍団兵"],
+      "Cost":[5]
+    },
+    {
+      "Name":["剣闘士", "大金"],
+      "Cost":[3, [8, -8]]
+    },
+    {
+      "Name":["公共広場"],
+      "Cost":[5]
+    },
+    {
+      "Name":["市街"],
+      "Cost":[-8]
+    },
+    {
+      "Name":["資料庫"],
+      "Cost":[5]
+    },
+    {
+      "Name":["城"],
+      "Cost":[3]
+    },
+    {
+      "Name":["陣地", "鹵獲品"],
+      "Cost":[2,5]
+    },
+    {
+      "Name":["神殿"],
+      "Cost":[4]
+    },
+    {
+      "Name":["戦車競走"],
+      "Cost":[3]
+    },
+    {
+      "Name":["大君主"],
+      "Cost":[-8]
+    },
+    {
+      "Name":["庭師"],
+      "Cost":[5]
+    },
+    {
+      "Name":["農家の市場"],
+      "Cost":[3]
+    },
+    {
+      "Name":["元手"],
+      "Cost":[5]
+    },
+    {
+      "Name":["ワイルドハント"],
+      "Cost":[5]
+    },
+  ]
+};
+//暗黒時代
+var Ankoku = {
+  "ExtensionName":"暗黒時代",
+  "Cards": [
+    {
+      "Name":["祭壇"],
+      "Cost":[6]
+    },
+    {
+      "Name":["武器庫"],
+      "Cost":[4]
+    },
+    {
+      "Name":["はみだし者"],
+      "Cost":[5]
+    },
+    {
+      "Name":["山賊の宿営地"],
+      "Cost":[5]
+    },
+    {
+      "Name":["物乞い"],
+      "Cost":[2]
+    },
+    {
+      "Name":["地下墓所"],
+      "Cost":[5]
+    },
+    {
+      "Name":["伯爵"],
+      "Cost":[5]
+    },
+    {
+      "Name":["狂信者"],
+      "Cost":[5]
+    },
+    {
+      "Name":["死の荷車"],
+      "Cost":[4]
+    },
+    {
+      "Name":["採取者"],
+      "Cost":[3]
+    },
+    {
+      "Name":["城塞"],
+      "Cost":[4]
+    },
+    {
+      "Name":["墓暴き"],
+      "Cost":[5]
+    },
+    {
+      "Name":["隠遁者"],
+      "Cost":[3]
+    },
+    {
+      "Name":["狩場"],
+      "Cost":[6]
+    },
+    {
+      "Name":["金物商"],
+      "Cost":[4]
+    },
+    {
+      "Name":["屑屋"],
+      "Cost":[5]
+    },
+    {
+      "Name":["襲撃者"],
+      "Cost":[4]
+    },
+    {
+      "Name":["青空市場"],
+      "Cost":[3]
+    },
+    {
+      "Name":["秘術師"],
+      "Cost":[5]
+    },
+    {
+      "Name":["略奪"],
+      "Cost":[5]
+    },
+    {
+      "Name":["救貧院"],
+      "Cost":[1]
+    },
+    {
+      "Name":["行進"],
+      "Cost":[4]
+    },
+    {
+      "Name":["建て直し"],
+      "Cost":[5]
+    },
+    {
+      "Name":["盗賊"],
+      "Cost":[5]
+    },
+    {
+      "Name":["賢者"],
+      "Cost":[3]
+    },
+    {
+      "Name":["ゴミあさり"],
+      "Cost":[4]
+    },
+    {
+      "Name":["従者"],
+      "Cost":[2]
+    },
+    {
+      "Name":["物置"],
+      "Cost":[3]
+    },
+    {
+      "Name":["浮浪児"],
+      "Cost":[3]
+    },
+    {
+      "Name":["浮浪者"],
+      "Cost":[2]
+    },
+    {
+      "Name":["吟遊詩人"],
+      "Cost":[4]
+    },
+    {
+      "Name":["偽造通貨"],
+      "Cost":[5]
+    },
+    {
+      "Name":["封土"],
+      "Cost":[4]
+    },
+    {
+      "Name":["ネズミ"],
+      "Cost":[4]
+    },
+    {
+      "Name":["騎士"],
+      "Cost":[5]
+    }
+  ]
+};
+//繁栄
+var Hanei = {
+  "ExtensionName":"繁栄",
+  "Cards": [
+    {
+      "Name":["銀行"],
+      "Cost":[7]
+    },
+    {
+      "Name":["司教"],
+      "Cost":[4]
+    },
+    {
+      "Name":["都市"],
+      "Cost":[5]
+    },
+    {
+      "Name":["禁制品"],
+      "Cost":[5]
+    },
+    {
+      "Name":["会計所"],
+      "Cost":[5]
+    },
+    {
+      "Name":["拡張"],
+      "Cost":[7]
+    },
+    {
+      "Name":["鍛造"],
+      "Cost":[7]
+    },
+    {
+      "Name":["ならず者"],
+      "Cost":[6]
+    },
+    {
+      "Name":["大市場"],
+      "Cost":[6]
+    },
+    {
+      "Name":["隠し財宝"],
+      "Cost":[6]
+    },
+    {
+      "Name":["宮廷"],
+      "Cost":[7]
+    },
+    {
+      "Name":["借金"],
+      "Cost":[3]
+    },
+    {
+      "Name":["造幣所"],
+      "Cost":[5]
+    },
+    {
+      "Name":["記念碑"],
+      "Cost":[4]
+    },
+    {
+      "Name":["香具師"],
+      "Cost":[5]
+    },
+    {
+      "Name":["行商人"],
+      "Cost":[8]
+    },
+    {
+      "Name":["石切り場"],
+      "Cost":[4]
+    },
+    {
+      "Name":["大衆"],
+      "Cost":[5]
+    },
+    {
+      "Name":["玉璽"],
+      "Cost":[5]
+    },
+    {
+      "Name":["護符"],
+      "Cost":[4]
+    },
+    {
+      "Name":["交易路"],
+      "Cost":[3]
+    },
+    {
+      "Name":["保管庫"],
+      "Cost":[5]
+    },
+    {
+      "Name":["投機"],
+      "Cost":[5]
+    },
+    {
+      "Name":["望楼"],
+      "Cost":[3]
+    },
+    {
+      "Name":["労働者の村"],
+      "Cost":[4]
+    },
+  ]
+};
+//カードリスト終了
+var ExtensionList = {Teikoku:Teikoku, Ankoku:Ankoku, Hanei:Hanei};
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById('btn').addEventListener('click', function() {
+    var SelectExtensions = [];
+    var selectCards = [];
+    var extensions = document.getElementsByName('SelectExtension');
+
+    for (var i = 0, len = extensions.length; i < len; i++){
+      var extension = extensions.item(i);
+      if (extension.checked) {
+        SelectExtensions.push(extension.value)}
+    }
+    //console.log(SelectExtensions.toString());
+    //console.log(ExtensionList[SelectExtensions.toString()]);
+    for (var i = 0, len = SelectExtensions.length; i < len; i++){
+      //console.log(ExtensionList[SelectExtensions[i]].ExtensionName);
+          for (var j = 0, len2 = ExtensionList[SelectExtensions[i]].Cards.length; j < len2; j++){
+            selectCards.push([ExtensionList[SelectExtensions[i]].Cards[j].Name, ExtensionList[SelectExtensions[i]].Cards[j].Cost, ExtensionList[SelectExtensions[i]].ExtensionName]);
+            //console.log("拡張セット： "　+ ExtensionList[SelectExtensions[i]].ExtensionName);
+          }
+      }
+      console.log(selectCards.length);
+      var cardCount = selectCards.length;
+      let pickupIndex = new Set();
+      while(pickupIndex.size  <= 9){
+        pickupIndex.add(Math.floor(Math.random()*cardCount));
+      }
+      console.log(pickupIndex);
+      var lineindex = 1
+
+      for (var i of pickupIndex.values()){
+        //console.log(selectCards[i][2]);
+        var str = selectCards[i][0].toString();
+        var Cost = selectCards[i][1].toString();
+        var Extension = selectCards[i][2];
+        console.log(str);
+        str = str.replace(",", "/");
+        Cost = Cost.replace(",", "/");
+        if (lineindex%2) {
+          result.insertAdjacentHTML('afterend', "<div class='columns col-gapless'><div class='column col-6 bg-secondary'>"+ str + "</div><div class='column col-2 bg-secondary'>" + Cost + "</div><div class='column col-4 bg-secondary'>" + Extension + "</div></div>");
+        }
+        else{
+          result.insertAdjacentHTML('afterend', "<div class='columns col-gapless'><div class='column col-6 bg-gray'>"+ str + "</div><div class='column col-2 bg-gray'>" + Cost + "</div><div class='column col-4 bg-gray'>" + Extension + "</div></d  iv>");
+        }
+        lineindex++
+      }
+      result.insertAdjacentHTML('afterend', "<div class='columns col-gapless'><div class='column col-6 bg-primary text-light'>カード</div><div class='column col-2 bg-primary text-light'>コスト</div><div class='column col-4 bg-primary text-light'>拡張セット</div></div>");
+
+      //for (var i = 0, len = pickupIndex.length; i < len; i++){      }
+    //}
+  }, false);
+}, false);
+
+
+/*
+var cardCount = 25;
+let pickupIndex = new Set();
+while(pickupIndex.size  <= 9){
+  pickupIndex.add(Math.floor(Math.random()*cardCount));
+}
+console.log(pickupIndex);
+*/
